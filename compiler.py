36da3ec3ac5d9c332230ec1a548cae5d570978ab
Cristian B) <cristian.ramirez@necsus.com.mx>
2024-07-11 00:08:45 -0600

    Add support to list

--- compiler.py
+++ compiler.py
@@ -1,4 +1,6 @@
 from lark import Lark, Transformer, Tree
+import sys
+import json
 
 grammar = """
 start: element+
@@ -9,19 +11,25 @@ element: equation
      | block_math
      | TEXT
      | new_line
+     | order_list
+     | unorder_list
 
-equation: "$" TEXT "$"
+equation: "$" TEXT+ "$"
 block_math: "$$" TEXT+ "$$"
 
-note: "\\begin{note}{" TEXT "}" content_note+ "\\end{note}"
+note: "@note{" TEXT "}{" content_note+ "}"
 content_note: (equation | TEXT | new_line)*
 
-block: "\\begin{block}" item+ "\\end{block}"
-item: "\\begin{item}" (TEXT | equation | custom_note | new_line)+ "\\end{item}"
+block: "@begin{block}" item+ "@end{block}"
+item: "@item" (TEXT | equation | note | new_line)*
 
-new_line: "@" "newline"
+new_line: "\\newline" | "\\\\" | "@newline"
 
 TEXT: /[^<>{}$@|]+/
+
+order_list: "@begin{enumerate}" item_list+ "@end{enumerate}"
+unorder_list: "@begin{itemize}" item_list+ "@end{itemize}"
+item_list: "@item" (equation | TEXT | new_line | note | block_math)*
 
 %import common.WS
 %ignore WS
@@ -39,7 +47,7 @@ class TreeToJSON(Transformer):
   def equation(self, items):
     return {"type": "equation", "content": items[0]}
 
-  def custom_note(self, items):
+  def note(self, items):
     title, content = items
     return {"type": "note", "title": title, "content": content}
 
@@ -54,27 +62,43 @@ class TreeToJSON(Transformer):
 
   def item(self, items):
     return {"type": "item", "content": items}
-  
+
   def new_line(self, items):
     return {"type": "new_line"}
 
   def TEXT(self, items):
     return {"type": "text", "content": items}
 
+  def order_list(self, items):
+    return {"type": "order_list", "content": items}
+
+  def unorder_list(self, items):
+    return {"type": "unorder_list", "content": items}
+
+  def item_list(self, items):
+    return {"type": "item_list", "content": items}
+
 def compile_to_json(source_code):
   parser = Lark(grammar, parser='lalr', transformer=TreeToJSON())
   tree = parser.parse(source_code)
   return tree
 
+def cleaner(source):
+    aux = source.replace("\\\\", "\\newline")
+    aux = aux.replace("\\begin", "@begin")
+    aux = aux.replace("\\end", "@end")
+    aux = aux.replace("\\note", "@note")
+    aux = aux.replace("\\item", "@item")
+    return aux
+
 if __name__ == "__main__":
-  import json
-  with open("./examples/input.txt", "r") as file:
+  with open(sys.argv[1], "r") as file:
     source_code = file.read()
   
-  source_code = source_code.replace("\\\\", "@newline")
-  source_code = source_code.replace("\\newline", "@newline")
+  source_code = cleaner(source_code)
 
   json_data = compile_to_json(source_code)
   with open("output.json", "w") as file:
     json.dump(json_data, file, indent=4)
   print("Compilation successful.")
+
